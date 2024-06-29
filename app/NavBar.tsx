c2d65f4501fed2780469e8b3c96f636c761bb3fa
Khapham <khar22102003@gmail.com>
2024-06-29 13:45:51 +0700

    Style the active link in navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,18 +1,36 @@
+"use client";
+import classNames from "classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { FaBug } from "react-icons/fa";
 
 const NavBar = () => {
+  // usePathname is dependent on browser API
+  const currentPath = usePathname();
   const links = [
-    {label: 'Dashboard', link: '/'},
-    {label: 'Issues', link: '/'},
-  ]
+    { label: "Dashboard", link: "/" },
+    { label: "Issues", link: "/issues" },
+  ];
   return (
-    <nav className="flex space-x-6 border-b px-3 h-14 mb-6 items-center"> 
-      <Link href="/"><FaBug/></Link>
+    <nav className="flex space-x-6 border-b px-3 h-14 mb-6 items-center">
+      <Link href="/">
+        <FaBug />
+      </Link>
       <ul className="flex space-x-6">
-        {links.map(link => <li key={link.label}>
-          <Link className="text-zinc-500 hover:text-zinc-200 transition-colors" href={link.link}>{link.label}</Link>
-        </li>)}
+        {links.map((link) => (
+          <li key={link.label}>
+            <Link
+              className={classNames({
+                "text-zinc-100": link.link === currentPath,
+                "text-zinc-500": link.link !== currentPath,
+                "hover:text-zinc-200 transition-colors": true,
+              })}
+              href={link.link}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
