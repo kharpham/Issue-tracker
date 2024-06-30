2c7bf457f5a616a507133bd3ea9b16bf3c7bfe4e
Khapham <khar22102003@gmail.com>
2024-06-30 10:31:03 +0700

    Loading skeleton with Radix UI

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,7 +1,8 @@
 import { Table } from '@radix-ui/themes'
 import React from 'react'
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
+// import Skeleton from 'react-loading-skeleton'
+import { Skeleton } from '@radix-ui/themes'
+// import 'react-loading-skeleton/dist/skeleton.css'
 import IssueActions from './IssueActions'
 
 const LoadingIssuesPage = () => {
