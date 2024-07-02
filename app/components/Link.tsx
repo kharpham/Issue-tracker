6c9a19188550ee3bc6d25f3008e3ddc9304ccfd8
Khapham <khar22102003@gmail.com>
2024-07-02 09:35:06 +0700

    Build styled link component

--- app/components/Link.tsx
+++ app/components/Link.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const Link = ({href, children}: Props) => {
   return (  
-    <NextLink href={href}><RadixLink>{children}</RadixLink></NextLink>
+    <NextLink href={href} passHref legacyBehavior><RadixLink>{children}</RadixLink></NextLink>
   )
 }
 
