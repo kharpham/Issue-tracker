d8335047d4d6d3ca57470f3252e7329089e4c26d
Khapham <khar22102003@gmail.com>
2024-07-03 20:53:16 +0700

    Enhance API to patch issues

--- app/api/issues/[id]/route.tsx
+++ app/api/issues/[id]/route.tsx
@@ -1,28 +1,35 @@
-import { fetchSingleIssue } from "@/app/issues/[id]/fetchSingleIssue";
-import { issueSchema } from "@/app/validationSchemas";
-import prisma from "@/prisma/client";
-import { NextRequest, NextResponse } from "next/server";
-import secureAuth from "../secureAuth";
-import { getServerSession } from "next-auth";
 import authOptions from "@/app/auth/authOptions";
+import { fetchSingleIssue } from "@/app/issues/[id]/fetchSingleIssue";
+import { patchIssueSchema } from "@/app/validationSchemas";
+import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  const session = await getServerSession(authOptions);
-  if (!session) return NextResponse.json({}, { status: 401 });
+  // const session = await getServerSession(authOptions);
+  // if (!session) return NextResponse.json({}, { status: 401 });
   await fetchSingleIssue(params.id);
   const body = await request.json();
-  const result = issueSchema.safeParse(body);
+  const result = patchIssueSchema.safeParse(body);
   if (!result.success)
     return NextResponse.json({ error: result.error.errors }, { status: 400 });
+  const { assignedToUserId, description, status, title } = result.data;
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({ where: { id: assignedToUserId } });
+    if (!user) return NextResponse.json({ error: "invalid user" }, { status: 400 });
+  }
+
+  // Only update the fields that are provided
   const updatedIssue = await prisma.issue.update({
     where: { id: parseInt(params.id) },
     data: {
-      title: result.data.title,
-      description: result.data.description,
-      status: result.data.status,
+      title,
+      description,
+      status,
+      assignedToUserId
     },
   });
   return NextResponse.json(updatedIssue);
--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -1,8 +1,32 @@
-import {z} from 'zod';
+import { z } from "zod";
 
 export const issueSchema = z.object({
-    title: z.string().min(1, "Title is required.").max(255, "Title must be at most 255 characters."),
-    description: z.string().min(1, "Description is required."),
-    status: z.enum(["OPEN", "IN_PROGRESS", "CLOSED"]).optional(),
-})
+  title: z
+    .string()
+    .min(1, "Title is required.")
+    .max(255, "Title must be at most 255 characters."),
+  description: z.string().min(1, "Description is required.").max(65535),
+  status: z.enum(["OPEN", "IN_PROGRESS", "CLOSED"]).optional(),
+});
+
 export type IssueFormData = z.infer<typeof issueSchema>;
+
+export const patchIssueSchema = z.object({
+  title: z
+    .string()
+    .min(1, "Title is required.")
+    .max(255, "Title must be at most 255 characters.")
+    .optional(),
+  description: z
+    .string()
+    .min(1, "Description is required.")
+    .max(65535)
+    .optional(),
+  status: z.enum(["OPEN", "IN_PROGRESS", "CLOSED"]).optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, "AssignedToUserId is required.")
+    .max(255)
+    .optional()
+    .nullable(),
+});
