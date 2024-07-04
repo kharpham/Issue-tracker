65c4c382484c0f3cefd966ce50594e495e933df9
Khapham <khar22102003@gmail.com>
2024-07-04 13:10:11 +0700

    Build the latest issues component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import Pagination from "./components/Pagination";
+import LatestIssues from "./LatestIssues";
 
-export default function Home({searchParams}: {searchParams: {page: string}}) {
-  return <div>Hello world</div>
+export default function Home() {
+  return (
+    <>
+      <LatestIssues />
+    </>
+  );
 }
