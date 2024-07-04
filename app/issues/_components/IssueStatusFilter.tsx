b1db9bad0c244ef6f51ff51bb23003ba4089d873
Khapham <khar22102003@gmail.com>
2024-07-04 10:46:16 +0700

    Fixing filtering bugs

--- app/issues/_components/IssueStatusFilter.tsx
+++ app/issues/_components/IssueStatusFilter.tsx
@@ -1,31 +1,39 @@
-"use client"
-import { IssueStatusBadge } from '@/app/components'
-import { Status } from '@prisma/client'
-import { Select } from '@radix-ui/themes'
-import { useRouter } from 'next/navigation'
-import React from 'react'
+"use client";
+import { Status } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import { useRouter, useSearchParams } from "next/navigation";
 
-const statuses: {label: string, value?: Status}[] = [
-  {label: 'All'},
-  {label: "Open", value: "OPEN"},
-  {label: "Closed", value: "CLOSED"},
-  {label: "In Progress", value: "IN_PROGRESS"}
-]
+const statuses: { label: string; value?: Status }[] = [
+  { label: "All" },
+  { label: "Open", value: "OPEN" },
+  { label: "Closed", value: "CLOSED" },
+  { label: "In Progress", value: "IN_PROGRESS" },
+];
 
 const IssueStatusFilter = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
   return (
-    <Select.Root onValueChange={(status) => {
-      const query = status !== "unassigned" ? `?status=${status}` : "";
-      router.push("/issues" + query);
-    }}>
-        <Select.Trigger placeholder='Filter by status...'/>
-        <Select.Content>
-          {statuses.map(status => <Select.Item key={status.value} value={status.value || "unassigned"}>{status.label}</Select.Item>)}
-        </Select.Content>
-        
+    <Select.Root defaultValue={searchParams.get('status') || ""}
+      onValueChange={(status) => {
+        const params = new URLSearchParams();
+        if (status) params.append("status", status);
+        if (searchParams.get("orderBy"))
+          params.append("orderBy", searchParams.get("orderBy")!);
+        const query = params.size ? '?' + params.toString() : ""
+        router.push("/issues" + query);
+      }}
+    >
+      <Select.Trigger placeholder="Filter by status..." />
+      <Select.Content>
+        {statuses.map((status) => (
+          <Select.Item key={status.value} value={status.value || "all"}>
+            {status.label}
+          </Select.Item>
+        ))}
+      </Select.Content>
     </Select.Root>
-  )
-}
+  );
+};
 
-export default IssueStatusFilter
+export default IssueStatusFilter;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -22,10 +22,12 @@ const IssuesPage = async ({
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
+  const orderBy = columns.map(column => column.value).includes(searchParams.orderBy) ? {[searchParams.orderBy]: 'asc'} : undefined;
   const data = await prisma.issue.findMany({
     where: {
       status: status,
     },
+    orderBy
   });
   return (
     <>
